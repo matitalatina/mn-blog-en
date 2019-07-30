ad0c4727c51835c44b8464664929a3ec3bb2c9d9
Mattia Natali <mattia.natali@pirelli.com>
2019-07-30 18:36:28 +0200

    Fix url

--- src/website-config.ts
+++ src/website-config.ts
@@ -62,7 +62,7 @@ const config: WebsiteConfig = {
   coverImage: 'img/blog-cover.jpg',
   logo: 'img/ghost-logo.png',
   lang: 'it',
-  siteUrl: 'https://mattianatali.netlify.com',
+  siteUrl: 'https://www.mattianatali.it',
   facebook: 'https://www.facebook.com/mattia.natali',
   twitter: 'https://twitter.com/matitalatina',
   linkedin: 'https://www.linkedin.com/in/mattian/',
