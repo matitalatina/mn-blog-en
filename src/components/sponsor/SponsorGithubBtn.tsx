1b72c4929ddd9992bd7a53cf348aeada5618938e
Mattia Natali <matitalatina@gmail.com>
2021-01-09 18:35:35 +0100

    Fix button color

--- src/components/sponsor/SponsorGithubBtn.tsx
+++ src/components/sponsor/SponsorGithubBtn.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { btnGradient, ButtonLink } from '../ui/Button';
+import { btnColor, ButtonLink } from '../ui/Button';
 import { colors } from '../../styles/colors';
 
 export const SponsorGithubBtn: React.FC<{className?: string}> = ({ className }) => {
@@ -8,5 +8,5 @@ export const SponsorGithubBtn: React.FC<{className?: string}> = ({ className }) 
 };
 
 const ButtonFlex = styled(ButtonLink)`
-  background: ${btnGradient(colors.pink)}
+  ${btnColor(colors.pink)}
 `;
